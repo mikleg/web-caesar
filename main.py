67b8c638882c782f7d650e4b3bf1f3900368923c
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-06-11 13:34:50 -0700

    basic form copied

---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+page_header = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+"""    
+form = """
+      <!-- create your form here -->
+    <form action="/crypt" method="post">
+        <label for="new-movie">
+            I want to add
+            <input type="text" id="new-movie" name="new-movie"/>
+            to my watchlist.
+        </label>
+        <input type="submit" value="Add It"/>
+    </form>
+"""
+page_footer = """
+    </body>
+</html>
+"""
+@app.route("/crypt", methods=['POST'])
+def add_movie():
+    new_movie = request.form['new-movie']
+
+    # build response content
+    new_movie_element = "<strong>" + new_movie + "</strong>"
+    sentence = new_movie_element + " has been added to your Watchlist!"
+    content = page_header + "<p>" + sentence + "</p>" + page_footer
+
+    return content
+
+
+@app.route("/")
+def index():
+    content = page_header + form + page_footer
+    return content
+
+app.run()
