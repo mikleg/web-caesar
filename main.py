a90ad637a01c7facbdb7fd730ae1bddb0c0b42e9
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-06-11 19:36:41 -0700

    final version

--- main.py
+++ main.py
@@ -17,62 +17,44 @@ page_header = """
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-                list-style-type: none;
+                
             }
-            #textarea {
-                margin: 0 auto;
-                width: 510px;
+            textarea {
+                margin: 10px 0;
+                width: 540px;
                 height: 120px;
+
             }
         </style>
     </head>
     <body>
-"""    
+"""
 form = """
       <!-- create your form here -->
     <form action="/" method="post">
-        <ul style="list-style-type:none;">
-            <li>
-                <label for="rot">
-                    Rotate by:
-                    <input type="text" id="rot" name="rot" value="0"/>
-                </label>
-            </li>
-            <li>
-                <input type="text-area" id="textarea" name="text"/>
-            </li>
-            <li>
-                <input type="submit" value="Send"/>
-            </li>
-        </ul>
+        <label for="rot">
+            Rotate by:
+            <input type="text" id="rot" name="rot" value="0"/>
+        </label>
+        <br>
+        <textarea type="text" name="text">{0}</textarea>
+        <br>
+        <input type="submit"/>
+            
     </form>
 """
 page_footer = """
     </body>
 </html>
 """
-'''@app.route("/", methods=['POST'])
-def add_movie():
-    new_movie = request.form['rot']
-
-    # build response content
-    new_movie_element = "<strong>" + new_movie + "</strong>"
-    sentence = new_movie_element + " has been added to your Watchlist!"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
-
-    return content'''
-
 @app.route("/", methods=['POST'])
 def encrypt():
     rotation = int(request.form['rot'])
     text = request.form['text']
-    sentence = "<h1>" + rotate_string(text, rotation) + "</h1>"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
-    return content
+    return page_header+form.format(rotate_string(text, rotation))+page_footer
 
 @app.route("/")
 def index():
-    content = page_header + form + page_footer
-    return content
+    return page_header+form.format("")+page_footer
 
 app.run()
